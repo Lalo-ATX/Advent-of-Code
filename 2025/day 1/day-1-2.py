c3911dc79189f862877faa0d55f80bcf7fe3a7bb
Lalo ATX <ed@eaohana.com>
2025-12-02 14:28:11 -0600

    slight code tightening

--- 2025/day 1/day-1-2.py
+++ 2025/day 1/day-1-2.py
@@ -1,5 +1,6 @@
 INPUT_FILE = 'day-1 input.txt'
 DIAL_SIZE = 100
+DIRECTION_MAP = {'L': -1, 'R': 1}
 
 dial_before = 50
 dial_after = 50
@@ -7,10 +8,11 @@ zero_count = 0
 
 with open(INPUT_FILE) as f:
     for line in f:
-        direction_str = line[0]
-        direction_val = 1 if direction_str == 'R' else -1
+        direction_val = DIRECTION_MAP[line[0]]
         movement = direction_val * int(line[1:])
+
         dial_after = dial_before + movement
+
         # no need to check for a clockwise zero-cross because dial_before is always positive
         zero_cross = 1 if dial_before != 0 and dial_after <= 0 else 0
 
