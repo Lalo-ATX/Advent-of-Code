9c0357d3099fd43b8baab1bf123396f5e74ada58
Lalo ATX <ed@eaohana.com>
2025-12-05 21:10:08 -0600

    day 5 done. part 2 was just trivial

---
+++ 2025/day 5/day-5-2.py
@@ -0,0 +1,71 @@
+from typing import NamedTuple
+
+class Bookends(NamedTuple):
+    low_end: int
+    high_end: int
+
+INPUT_FILE = 'day-5 input.txt'
+
+# 1: below, below
+# 2: below, between
+# 3: below, above
+# 4: between, between
+# 5: between, above
+# 6: above, above
+
+def add_range_really(new_bookend: Bookends, range_db: list[Bookends]) -> None:
+#    print(f"testing {new_bookend.low_end}-{new_bookend.high_end} against {range_db}")
+    for idx, bookend in enumerate(range_db):
+        if new_bookend.low_end < bookend.low_end:
+            if new_bookend.high_end < bookend.low_end:
+                # 1: below, below
+                # insert ahead and return
+                range_db.insert(idx, new_bookend)
+                return
+
+            # 2: below, between
+            # 3: below: above
+            # else, new_bookend.high_end must be >= bookend.low_end
+            # delete the old bookend, expand it and re-add it
+            range_db.pop(idx)
+            add_range_really(Bookends(new_bookend.low_end, max([bookend.high_end, new_bookend.high_end])), range_db)
+            return
+
+        # new_bookend.low_end must be >= bookend.low_end
+        if new_bookend.low_end <= bookend.high_end:
+            # 4: between, between
+            # 5: between, above
+            range_db.pop(idx)
+            add_range_really(Bookends(bookend.low_end, max([bookend.high_end, new_bookend.high_end])), range_db)
+            return
+        
+        # else, continue
+    
+    # if we get here, that means we're #6, above, above all other entries
+    range_db.append(new_bookend)
+
+def add_range(id_range: str, range_db: list[Bookends]) -> None:
+    id_low, id_high = id_range.split("-")
+    new_bookend = Bookends(int(id_low), int(id_high))
+    add_range_really(new_bookend, range_db)
+
+def ranges_counts(range_db: list[Bookends]) -> int:
+    count = 0
+    for check_range in range_db:
+        count += check_range.high_end - check_range.low_end + 1
+
+    return count
+
+with open(INPUT_FILE) as f:
+    content = f.read()
+
+id_ranges, ids = content.split("\n\n")
+
+range_db: list[Bookends] = []
+
+for id_range in id_ranges.split("\n"):
+    add_range(id_range, range_db)
+
+# now just count the size of each range
+total_ids = ranges_counts(range_db)
+print(f"total fresh IDs: {total_ids}")
