a95b4d9e18d01f9141583cbe75dbbd51b0d11bf3
Lalo ATX <ed@eaohana.com>
2025-12-07 15:39:08 -0600

    changed from set removal and addition methods to operators

--- 2025/day 7/day-7-1.py
+++ 2025/day 7/day-7-1.py
@@ -17,9 +17,8 @@ split_count = 0
 for row in tmanifold:
     for beam_pos in list(beams):
         if row[beam_pos] == '^':
-            beams.remove(beam_pos)
-            beams.add(beam_pos - 1)
-            beams.add(beam_pos + 1)
+            beams -= {beam_pos}
+            beams |= {beam_pos - 1, beam_pos + 1}
             split_count += 1
 
 print(f"split {split_count} times")
